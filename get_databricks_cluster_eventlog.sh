66f6f066c2f67126293c0c2a29a4dd6ddc219ed9
Sean Gorsky <gorsky@Seans-MacBook-Pro.local>
2022-08-08 14:59:54 -0400

    Validation of databricks cli installation

--- get_databricks_cluster_eventlog.sh
+++ get_databricks_cluster_eventlog.sh
@@ -33,6 +33,11 @@ function eventlog_cli_call {
     databricks clusters events --cluster-id $clusterid --output JSON --order ASC --offset $offset
 }
 
+# Check for databricks CLI installation
+if ! command -v databricks &> /dev/null
+then
+    echo "Databricks CLI could not be found. Please install and configure before running script."
+fi
 
 # Make initial databricks call
 call="$(eventlog_cli_call)"
