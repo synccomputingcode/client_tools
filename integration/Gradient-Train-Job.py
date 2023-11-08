4493d27231b8267c44e857a5a42e55246d6aa4fb
Vinoo Ganesh <vinoo.ganesh@gmail.com>
2023-11-07 21:50:07 -0500

    ErrorHandling

--- integration/Gradient-Train-Job.py
+++ integration/Gradient-Train-Job.py
@@ -2,26 +2,15 @@
 # MAGIC %md
 # MAGIC #Train Gradient Model for Databricks Job
 # MAGIC This notebook executes a job multiple time in order to complete the Gradient Learning phase. The default setup assumes the following:
-# MAGIC   * The Gradient Webhook has been configured
-# MAGIC   * The Databricks Job has been Gradient enabled
-# MAGIC ## Update these values below
-# MAGIC   * DATABRICKS_HOST: Databricks host
-# MAGIC   * DATABRICKS_TOKEN: Databricks personal access token
-# MAGIC   * DATABRICKS_JOB_ID: Job ID to train
-# MAGIC   * TRAINING_RUNS: Number of training runs
+# MAGIC  * The Gradient Webhook has been configured
+# MAGIC  * The Databricks Job has been Gradient enabled
+# MAGIC
+# MAGIC This job will confirgure all runs to execute using ON DEMAND nodes only.  The orginal settings will be restored after training is complete.
 # MAGIC
 
 # COMMAND ----------
 
-# Update these values
-DATABRICKS_HOST = dbutils.secrets.get(scope="Sync Computing | 4fb27cc4-7e2b-45d6-82bc-0b3cc9b6025f", key="DATABRICKS_HOST")
-DATABRICKS_TOKEN = dbutils.secrets.get(scope="Sync Computing | 4fb27cc4-7e2b-45d6-82bc-0b3cc9b6025f", key="DATABRICKS_TOKEN")
-DATABRICKS_JOB_ID = 227554421094496
-TRAINING_RUNS = 2
-
-# COMMAND ----------
-
-# MAGIC %pip install databricks-sdk
+# MAGIC %pip install -I https://github.com/synccomputingcode/syncsparkpy/archive/latest.tar.gz
 
 # COMMAND ----------
 
@@ -29,64 +18,104 @@ dbutils.library.restartPython
 
 # COMMAND ----------
 
-import time
-from databricks.sdk import WorkspaceClient
-from databricks.sdk.service import workspace
-from databricks.sdk.service import jobs
-from databricks.sdk.service.compute import AutoScale
+# MAGIC %md
+# MAGIC ## Update these values below
+# MAGIC  * DATABRICKS_HOST: Databricks host
+# MAGIC  * DATABRICKS_TOKEN: Databricks personal access token
+# MAGIC  * DATABRICKS_JOB_ID: Job ID to train
+# MAGIC  * TRAINING_RUNS: Number of training runs
 
-w = WorkspaceClient(
-  host  = DATABRICKS_HOST,
-  token = DATABRICKS_TOKEN
-)
+# COMMAND ----------
 
-job = w.jobs.get(DATABRICKS_JOB_ID)
+import os
+# Update these values
+DATABRICKS_JOB_ID = 65609458830436
+TRAINING_RUNS = 2
 
-#Current Configuration
-print("Name:" + str(job.settings.job_clusters[0].job_cluster_key or ""))
-print("Num Workers:" + str(job.settings.job_clusters[0].new_cluster.num_workers or ""))
-print("Autoscale:" + str(job.settings.job_clusters[0].new_cluster.autoscale or ""))
-print("Original First On Demand:" + str(job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand or ""))
+os.environ["DATABRICKS_TOKEN"] = ""
+os.environ["SYNC_API_KEY_ID"] = ""
+os.environ["SYNC_API_KEY_SECRET"] = ""
+os.environ["DATABRICKS_HOST"] = "https://[EDITME].cloud.databricks.com"
 
-if job.settings.job_clusters[0].new_cluster.autoscale is not None:
-    autoscale = job.settings.job_clusters[0].new_cluster.autoscale.max_workers
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Configure all training runs to execute using ON DEMAND clusters
+
+# COMMAND ----------
+
+from sync.clients import databricks
+import orjson
+
+client = databricks.get_default_client()
+job = client.get_job(DATABRICKS_JOB_ID)
+
+print(job)
+
+#Find the original number of on-demand workers
+original_ondemand = job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["first_on_demand"] or 0
+original_availability = job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["availability"]
+print("Original First_on_demand:" + str(original_ondemand or ""))
+print("Original Availability: " + original_availability)
+
+#Update the cluster to all on-demand.  Answer will depend on if autoscaling is enabled
+if("autoscale" in job["settings"]["job_clusters"][0]["new_cluster"].keys()):
+    autoscale = job["settings"]["job_clusters"][0]["new_cluster"]["autoscale"]["max_workers"]
+    updated_ondemand = autoscale + 1
+    print("Updated First_on_demand:" + str(updated_ondemand or "")) 
 else:
     autoscale = 0
+    updated_ondemand = (job["settings"]["job_clusters"][0]["new_cluster"]["num_workers"] or 0) + 1
+    print("Updated First_on_demand:" + str(updated_ondemand or "")) 
 
-orgiginal_ondemand = job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand or 0
-ondemand = (job.settings.job_clusters[0].new_cluster.num_workers or 0) + autoscale + 1
-print("First_on_demand:" + str(ondemand or ""))
 
-job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand = ondemand
+#Update the first_on_demand to the new updated on-demand value
+job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["first_on_demand"] = updated_ondemand
+job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["availability"] = "ON_DEMAND"
 
-w.jobs.update(job_id=DATABRICKS_JOB_ID,new_settings=job.settings)
+#Update the cluster settings
+client.update_job(job_id=DATABRICKS_JOB_ID, new_settings=job)
 
-print("Num Workers:" + str(job.settings.job_clusters[0].new_cluster.num_workers or ""))
-print("Autoscale:" + str(job.settings.job_clusters[0].new_cluster.autoscale or ""))
-print("New First On Demand:" + str(job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand or ""))
+# COMMAND ----------
 
-def get_run_result_state(run_id):
-    run_state = w.jobs.get_run(run_id=run_id).state.life_cycle_state
-    result_state = w.jobs.get_run(run_id=run_id).state.result_state
+import json
+import requests
+import os
+import time
 
-    while (run_state.value not in ("TERMINATED", "SKIPPED", "INTERNAL_ERROR")):
+def run_job(run_job_id):
+    values = {'job_id': run_job_id}
+
+    resp = requests.post(os.getenv("DATABRICKS_HOST") + '/api/2.1/jobs/run-now',
+                            data=json.dumps(values), auth=("token", os.getenv("DATABRICKS_TOKEN")))
+    runjson = resp.text
+    print(resp)
+    d = json.loads(runjson)
+    print("run_id:" + str(d['run_id']))
+    runid = d['run_id']
+
+    waiting = True
+    while waiting:
+        time.sleep(60)
         print("...")
-        time.sleep(30)
-        result_state = w.jobs.get_run(run_id=run_id).state.result_state
-        run_state = w.jobs.get_run(run_id=run_id).state.life_cycle_state
-    else:
-        if (result_state is None):
-            return "UNSUCCESSFUL"
-        else:
-            return result_state.value
+        jobresp = requests.get(os.getenv("DATABRICKS_HOST") + '/api/2.1/jobs/runs/get?run_id='+str(runid),
+                            auth=("token", os.getenv("DATABRICKS_TOKEN")))
+        jobjson = jobresp.text
+        j = json.loads(jobjson)            
+        current_state = j['state']['life_cycle_state']
+        runid = j['run_id']
+        if current_state in ['TERMINATED', 'INTERNAL_ERROR', 'SKIPPED']:
+            termination_state = j['state']['result_state']
+            break
+    return termination_state
+    
 
-def submit_test_runs(job_id, training_runs):
+def submit_test_runs(submit_job_id, training_runs):
     current_run=1
     while current_run <= training_runs:
         print("Starting run " + str(current_run) + " of " + str(training_runs))
-        run = w.jobs.run_now(job_id=job_id)
-        print("run id:" + str(run.response.run_id))
-        termination_state = get_run_result_state(run.response.run_id)
+        termination_state = run_job(run_job_id=submit_job_id)
+        print("run status:" + termination_state)
         if termination_state == 'SUCCESS':
             print("Completed")
             current_run = current_run + 1
@@ -94,13 +123,19 @@ def submit_test_runs(job_id, training_runs):
             print("Unsuccessful run. Training Ended")
             current_run = training_runs + 1
 
-submit_test_runs(job_id=DATABRICKS_JOB_ID, training_runs=TRAINING_RUNS)
-
-job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand = orgiginal_ondemand
-w.jobs.update(job_id=DATABRICKS_JOB_ID,new_settings=job.settings)
-print("Restored First On Demand:" + str(job.settings.job_clusters[0].new_cluster.aws_attributes.first_on_demand or ""))
-
+submit_test_runs(submit_job_id=DATABRICKS_JOB_ID, training_runs=TRAINING_RUNS)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ###Restore original ON DEMAND settings
 
+# COMMAND ----------
+
+job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["first_on_demand"] = original_ondemand
+job["settings"]["job_clusters"][0]["new_cluster"]["aws_attributes"]["availability"] = original_availability
+print(job)
+original_settings = {"job_clusters": job["settings"]["job_clusters"]}
+print(original_settings)
+#client.update_job(job_id=DATABRICKS_JOB_ID, new_settings=original_settings)
+client.update_job(job_id=DATABRICKS_JOB_ID, new_settings=job)
