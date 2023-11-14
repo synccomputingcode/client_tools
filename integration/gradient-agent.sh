3b8de6ab26d10a03481f142cca380339c131c318
Scott <scott@synccomputing.com>
2023-11-14 13:36:34 -0800

    We don't need SYNC_PROJECT_ID in the init script (#28)

--- integration/gradient-agent.sh
+++ integration/gradient-agent.sh
@@ -38,25 +38,4 @@ print(get_default_client().get_platform().value)
 EOD
 )
 
-if [[ -z $SYNC_PROJECT_ID ]]; then
-  SYNC_PROJECT_ID=$(python <<EOD
-from sync.${PLATFORM/-/} import get_cluster
-from sync.api.projects import get_projects
-
-cluster_response = get_cluster("$DB_CLUSTER_ID")
-if cluster_response.result:
-  cluster = cluster_response.result
-  if cluster['cluster_source'] == "JOB":
-    project_id = cluster.get('custom_tags', {}).get('sync:project-id')
-    if project_id:
-      print(project_id)
-EOD
-)
-fi
-
-if [[ -z $SYNC_PROJECT_ID ]]; then
-  >&2 echo "No project found for job running on cluster $DB_CLUSTER_ID"
-  exit 1
-fi
-
 sync-cli --debug $PLATFORM monitor-cluster $DB_CLUSTER_ID & disown
